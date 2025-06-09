41ddc21e0f99911475bc135ddc49e41526b6222d
hboot <bobolity@163.com>
2025-06-09 09:04:40 +0800

    chore: 使用sv create 创建svelteKit项目

--- entry.js
+++
@@ -1,6 +0,0 @@
-import App from "./dist/bundle.js";
-import { mount } from "svelte";
-
-mount(App, {
-  target: document.getElementById("app"),
-});
--- loaders/svelte-loader.cjs
+++
@@ -1,6 +0,0 @@
-const { compile: SvelteCompile } = require("svelte/compiler");
-
-module.exports = function (source) {
-  let result = SvelteCompile(source, {});
-  return result.js.code;
-};
